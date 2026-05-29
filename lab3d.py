d5803db80397c5a6223de11c2f82671963384275
Muhammad Ahmed Khan <makhan128@myseneca.ca>
2026-05-29 12:52:37 -0400

    Added Lab3d

---
+++ lab3d.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Author ID: makhan128
+
+import os
+def free_space():
+    stream = os.popen("df -h | grep '/$' | awk '{print $4}'")
+    output = stream.read()
+
+    return output.strip()
+
+if __name__ == '__main__':
+    print(free_space())
